3355f1be6d85534e196bb2be57b2362c10f67417
Kublai <kublai.com@gmail.com>
2018-04-17 01:08:15 +0200

    laraMod4

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
         ],
     ];
 
-    /**
+    /*
      * The application's route middleware.
      *
      * These middleware may be assigned to groups or used individually.
